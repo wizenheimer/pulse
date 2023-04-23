c9c999cfce5022f5fe8c88287a546a14bcd429e5
kn4y4n <kn4y4nnn@gmail.com>
2023-04-23 12:04:46 +0530

    feat: add team accounts

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # locally installed apps
+    'users',
     'monitor',
 ]
 
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
---
+++ users/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from random import choice
+from string import ascii_uppercase
+
+from .managers import UserManager
+
+
+class Team(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # token for creating invites
+    token = models.CharField(max_length=255, db_index=True, null=True, blank=True)
+
+    def update_token(self):
+        # update token for the given model
+        self.token = "".join(choice(ascii_uppercase) for i in range(16))
+        return self.token
+
+    def save(self, *args, **kwargs):
+        # create a new token
+        token = self.update_token()
+        super(Team, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, db_index=True)
+    # has verified email address
+    is_verified = models.BooleanField(default=False)
+    # has an active account
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # team
+    teams = models.ManyToManyField(Team, through="TeamAssignment")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return str(self.email)
+
+
+class TeamAssignment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    # permissions
+    # can create invites
+    can_add_teammate = models.BooleanField(default=True)
+    # can remove users (aka admin)
+    can_remove_teammate = models.BooleanField(default=True)
+    # join date
+    begin_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"team:{str(self.team.id)} user:{str(self.user.id)}"
