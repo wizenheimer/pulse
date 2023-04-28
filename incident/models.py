c89dbfe0194f1e93124e4776c1f353c3218fc862
kn4y4n <kn4y4nnn@gmail.com>
2023-04-29 04:58:29 +0530

    feat: escalation models

--- incident/models.py
+++ incident/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Team
+from users.models import Team, User
 
 
 class Service(models.Model):
@@ -35,3 +35,25 @@ class Incident(models.Model):
 
     def __str__(self):
         return self.status
+
+
+class EscalationLevel(models.Model):
+    level_number = models.IntegerField()
+    time_interval = models.PositiveIntegerField(
+        help_text="Time interval (in minutes) for this escalation level"
+    )
+    users = models.ManyToManyField(
+        User, help_text="Users to be notified for this escalation level"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Escalation Level {self.level_number}"
+
+
+class EscalationPolicy(models.Model):
+    name = models.CharField(max_length=255)
+    escalation_levels = models.ManyToManyField(EscalationLevel)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
