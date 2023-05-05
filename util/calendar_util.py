8dc84866afff95199a26ef384547bf1f6d945c12
kn4y4n <kn4y4nnn@gmail.com>
2023-05-05 22:32:18 +0530

    feat: calendar syndication

---
+++ util/calendar_util.py
@@ -0,0 +1,54 @@
+import requests
+import arrow
+import pytz
+from ics import Calendar
+
+
+def get_on_call(
+    url=None,
+    date=arrow.now(),
+    timezone="utc",
+    shift_day=0,
+    shift_hour=0,
+    shift_minute=0,
+    shift_second=0,
+):
+    """
+    The function queries the calendar and returns a timezone aware on-call list
+    We can even specify the time difference with number of hours instead of timezone eg '-05:00'
+    """
+    # take the timezone
+    if url is None:
+        return []
+
+    calendar = Calendar(requests.get(url).text)
+    # convert the current date to a date in the calendar timezone
+    begin_date = arrow.get(date).to(timezone)
+
+    # fetch the timeline at the specified date
+    timeline = calendar.timeline.at(begin_date).shift(
+        days=shift_day,
+        hours=shift_hour,
+        minutes=shift_minute,
+        seconds=shift_second,
+    )
+
+    on_call_list = []
+    for event in timeline:
+        # prepare the call list for the timeline
+        on_call_list.append(event.name)
+    return on_call_list
+
+
+def get_arrow_date(year, month, day, hour, minute, seconds, timezone="utc"):
+    """
+    Return the date as an arrow object
+    """
+    return arrow.Arrow(year, month, day, hour, minute, seconds).to(timezone)
+
+
+def get_arrow_date(date, timezone="utc"):
+    """
+    Parses the date and return an arrow object
+    """
+    return arrow.get(date, "MMMM DD YYYY HH:mm").to(timezone)
