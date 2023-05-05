5c238b8d89959367fed5653786a84e65db50a4dd
kn4y4n <kn4y4nnn@gmail.com>
2023-05-06 01:44:47 +0530

    fix: calendar date parsing

--- incident/views.py
+++ incident/views.py
@@ -1,6 +1,6 @@
 from users.models import User
 from rest_framework import viewsets
-from util.calendar_util import get_on_call
+from util.calendar_util import get_on_call, get_arrow_date
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import render, get_object_or_404
@@ -19,18 +19,34 @@ class OnCallCalendarViewset(viewsets.ModelViewSet):
     def users(self, request, pk=None):
         instance = get_object_or_404(OnCallCalendar, pk=pk)
         shift_day = request.query_params.get("shift_day", 0)
+        date = request.query_params.get("date", None)
         shift_hour = request.query_params.get("shift_hour", 0)
         shift_minute = request.query_params.get("shift_minute", 0)
         shift_second = request.query_params.get("shift_second", 0)
+
         user_id = []
-        emails = get_on_call(
-            url=instance.url,
-            timezone=instance.timezone,
-            shift_day=shift_day,
-            shift_hour=shift_hour,
-            shift_minute=shift_minute,
-            shift_second=shift_second,
-        )
+        emails = []
+        if date is not None:
+            date = get_arrow_date(date, timezone=instance.timezone)
+            emails = get_on_call(
+                url=instance.url,
+                date=date,
+                timezone=instance.timezone,
+                shift_day=shift_day,
+                shift_hour=shift_hour,
+                shift_minute=shift_minute,
+                shift_second=shift_second,
+            )
+        else:
+            emails = get_on_call(
+                url=instance.url,
+                timezone=instance.timezone,
+                shift_day=shift_day,
+                shift_hour=shift_hour,
+                shift_minute=shift_minute,
+                shift_second=shift_second,
+            )
+
         for email in emails:
             try:
                 user = User.objects.get(email=email)
